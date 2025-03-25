9b86d34863cc2ea4029d29bccd2f99f7fd45e755
noakwollsater <142594779+noakwollsater@users.noreply.github.com>
2025-03-26 00:29:08 +0100

    Meenennnuuu

---
+++ Assets/Noak/Script/Menu/FadeToBlackScript.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeToBlackScript : MonoBehaviour
+{
+    public GameObject fadeToBlack;
+    public Button[] buttonToUse;
+
+    void Start()
+    {
+        fadeToBlack.SetActive(false); // Hide the fade panel at the start
+        findButns();
+    }
+
+    private void findButns()
+    {
+        // Find all buttons with "Screen" in the name
+        buttonToUse = FindObjectsOfType<Button>().Where(b => b.name.Contains("Screen")).ToArray();
+
+        // Add the fade trigger to each button
+        foreach (Button button in buttonToUse)
+        {
+            button.onClick.AddListener(() => StartCoroutine(ToggleFade()));
+        }
+    }
+
+    private IEnumerator ToggleFade()
+    {
+        fadeToBlack.SetActive(true);
+        findButns();
+        yield return new WaitForSeconds(0.5f); // Wait for 4 seconds
+        fadeToBlack.SetActive(false);
+    }
+}
