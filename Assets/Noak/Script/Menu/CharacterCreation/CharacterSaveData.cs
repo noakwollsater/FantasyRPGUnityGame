2300e4f5fa572e7db3e0f23f57c151754e5634a8
noakwollsater <142594779+noakwollsater@users.noreply.github.com>
2025-03-26 22:46:32 +0100

    finnish off

--- Assets/Noak/Script/Menu/CharacterCreation/CharacterSaveData.cs
+++ Assets/Noak/Script/Menu/CharacterCreation/CharacterSaveData.cs
@@ -9,6 +9,20 @@ public class CharacterSaveData
     public string race;
     public string className;
 
+    public int level = 1;
+    public float experience = 0;
+    public float experienceToNextLevel = 100;
+
+    public string title = "Nobody";
+    public string alignment = "Neutral good";
+
+    public int bank = 0;
+    public int gold = 0;
+    public int silver = 0;
+    public int copper = 0;
+
+    public List<string> completedQuests = new();
+
     public float muscle;
     public float skinny;
     public float fat;
