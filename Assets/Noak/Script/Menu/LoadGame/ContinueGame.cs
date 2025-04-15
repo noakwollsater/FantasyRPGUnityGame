386cd5405ad9bf2b80a645e640ace4019244ff38
noakwollsater <142594779+noakwollsater@users.noreply.github.com>
2025-04-15 08:25:50 +0200

    Fixed continueGame function

---
+++ Assets/Noak/Script/Menu/LoadGame/ContinueGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unity.FantasyKingdom
+{
+    public class ContinueGame : MonoBehaviour
+    {
+        [SerializeField] private Button continueGameBtn;
+
+        void Start()
+        {
+            string lastSavedGameFile = PlayerPrefs.GetString("LastSavedGame", string.Empty);
+            string charactername = PlayerPrefs.GetString("SavedCharacterName", "Default");
+
+            if (string.IsNullOrEmpty(charactername))
+            {
+                continueGameBtn.interactable = false;
+                Debug.Log("⚠️ Ingen sparad fil hittades.");
+            }
+            else
+            {
+                continueGameBtn.interactable = true;
+                Debug.Log($"✅ Sparad fil hittades: {lastSavedGameFile}");
+                Debug.Log($"✅ Sparad karaktär hittades: {charactername}");
+                continueGameBtn.onClick.AddListener(OnClick);
+            }
+        }
+
+        private void OnClick()
+        {
+            SceneTransitionManager.Instance.LoadScene("Gameplay");
+        }
+    }
+}
